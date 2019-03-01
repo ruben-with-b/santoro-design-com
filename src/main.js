089b2fcf1078fd340f74cab6e815f6629a00f07d
Ruben <ruben.santoro@hs-augsburg.de>
2019-03-01 12:38:07 +0100

    Setup Vue Cli

--- src/main.js
+++ src/main.js
@@ -1,8 +1,13 @@
 import Vue from 'vue'
 import App from './App.vue'
+import Home from './Home.vue'
 
 Vue.config.productionTip = false
 
 new Vue({
   render: h => h(App),
 }).$mount('#app')
+
+new Vue({
+  render: h => h(Home),
+}).$mount('#home')
