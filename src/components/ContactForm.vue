b58bff5a7dd5489a8d5b5bf48d99f85e999bad25
rub-n <ruben.santoro@hs-augsburg.de>
2020-01-28 00:23:13 +0100

    Style contact form

--- src/components/ContactForm.vue
+++ src/components/ContactForm.vue
@@ -5,24 +5,45 @@
       <h1 class="is-family-secondary santoro-breadcrumb is-hidden-touch">{{ msg }}</h1>
       
 
-      <div class="sign-body">
-        <h1 class="sign-headline">Sign Up</h1>
+      <div class="form-body">
         <form action="" method="POST">
           <div class="input-group">
             <input type="text" id="userField" v-model="userData.username" required class="input-area">
-            <label for="userField" class="labelUser">Username</label>
+            <label for="userField" class="labelName">Vorname Nachname</label>
+          </div>
+          <div class="input-group">
+            <input type="text" id="companyField" v-model="userData.password" required class="input-area">
+            <label for="companyField" class="labelCompany">Firma (optional)</label>
+          </div>
+          <div class="input-group">
+            <input type="text" id="siteField" required class="input-area">
+            <label for="siteField" class="labelSite">Homepage (optional)</label>
           </div>
           <div class="input-group">
             <input type="email" id="mailField" v-model="userData.mail" required class="input-area">
             <label for="mailField" class="labelMail">E-Mail</label>
           </div>
           <div class="input-group">
-            <input type="password" id="passwordField" v-model="userData.password" required class="input-area">
-            <label for="passwordField" class="labelPassword">Password</label>
-            <router-link class="login" to="/login">
-              Login &nbsp;›
-            </router-link>
+            <input type="text" id="subjectField" required class="input-area">
+            <label for="subjectField" class="labelSubject">Betreff</label>
           </div>
+          <div class="input-group">
+            <textarea name="customerMessage" cols="50" rows="10" class="input-area text-area"
+            placeholder="Ihre Anfrage"/>
+            <label for="customerMessage" class="labelMessage">Nachricht</label>
+          </div>
+          <button class="button button-santoro-white">
+            <router-link to="/contact" class="menu-link is-secondary is-family-secondary">
+              <div class="arrow-mask">  
+                <icon-base class="santoro-arrow" width="1" height="1"
+                  viewBox="0 0 616.49 167.02" icon-name="arrow"
+                  iconColor="#FFFFFF">
+                  <icon-arrow />
+                </icon-base>
+              </div>
+              <span class="next-to-icon">Abschicken</span>
+            </router-link>
+          </button>
           <div v-if="responseNotOk" class="error-box">
             <p>
               Ouch... Any input missing?<br>
@@ -71,60 +92,16 @@ export default {
   background: #ffffff;
 }
 
-.sign-body{
-  display: flex;
-  align-items: center;
-  flex-direction: column;
-  color: #ffffff;
-  width: 100vw;
-  height: 85vh;
-
-  .sign-headline{
-    margin: 2em 0 2em 0;
-  }
-
-  .input-group{
-    display: flex;
-    flex-direction: column;
-    margin: 0 0 1em 0;
-    width: 18em;
-
-    .input-area{
-      outline: none;
-      padding: 0.5em 0;
-      border: none;
-      background: none;
-      border-bottom: 2px solid #ffffff;
-      font-size: 1.1rem;
-      &:focus{
-        border-bottom: 2px solid orange;
-      }
-      &:focus + .labelUser,
-      &:focus + .labelMail,
-      &:focus + .labelPassword{
-        top: -4em;
-        font-size: 0.9rem;
-      }
-      &:valid + .labelUser,
-      &:valid + .labelPassword,
-      &:valid + .labelMail{
-        top: -4em;
-        font-size: 0.9rem;
-      }
-    }
-
-    .labelUser,
-    .labelMail,
-    .labelPassword{
-      position: relative;
-      top: -2em;
-      font-size: 1.1rem;
-      transition: 100ms all ease-in-out;
-    }
-  }
-  .error-box{
-    color: orange;
-    margin: 4em 0 0 0;
-  }
+input:-webkit-autofill,
+input:-webkit-autofill:hover, 
+input:-webkit-autofill:focus,
+textarea:-webkit-autofill,
+textarea:-webkit-autofill:hover,
+textarea:-webkit-autofill:focus,
+select:-webkit-autofill,
+select:-webkit-autofill:hover,
+select:-webkit-autofill:focus {
+  box-shadow: 0 0 0em 100em #0a0a0a inset;
+  -webkit-text-fill-color: #F69F19;
 }
 </style>
