584b0ce9fae41dd693bb320d2fd0c7f2407d1fce
rub-n <ruben.santoro@hs-augsburg.de>
2020-05-14 21:11:11 +0200

    add Erinnerungs-Poster + Add Framework icons

--- src/components/WebsitesApps.vue
+++ src/components/WebsitesApps.vue
@@ -5,7 +5,7 @@
         <p class="is-family-primary has-text-black p-after-h p-anim">
           Hier sehen Sie eine Auswahl an Projekten, die individuell von mir oder in Zusammenarbeit mit anderen Studenten entstanden sind.
         </p>
-        <hooper ref="carousel" :settings="settings" class="sp-carousel">
+        <hooper ref="carouselSp" :settings="settings" class="three-device-carousel">
           <slide class="mockup-slide is-flex hcenter">
             <video-mockup-mobile class="desktop-mockup is-hidden-tablet"
               videoUrl="sp-widescreen"
@@ -70,10 +70,10 @@
           </slide>
         </hooper>
         <div class="is-flex arrow-hoo-nav">
-          <icon-base @click.native="slidePrev" width="50" height="50" viewBox="0 0 616.49 167.02" icon-name="arrow-back">
+          <icon-base @click.native="slidePrev" id="sweety-pretty" width="50" height="50" viewBox="0 0 616.49 167.02" icon-name="arrow-back">
             <icon-arrow-back />
           </icon-base>
-          <icon-base @click.native="slideNext" width="50" height="50" viewBox="0 0 616.49 167.02" icon-name="arrow-back">
+          <icon-base @click.native="slideNext" id="sweety-pretty" width="50" height="50" viewBox="0 0 616.49 167.02" icon-name="arrow-back">
             <icon-arrow-back />
           </icon-base>
         </div>
@@ -87,16 +87,161 @@
         </p>
         <div class="tools">
           <p>Frameworks</p>
-          <icon-base width="30" height="30" viewBox="0 0 100 100" icon-name="wordpress">
-            <icon-wordpress />
+          <icon-base width="30" height="30" viewBox="0 0 256 290" icon-name="vue-logo">
+            <icon-webpack />
+          </icon-base>
+          <icon-base width="30" height="30" viewBox="0 0 256 221" icon-name="vue-logo">
+            <icon-vue />
           </icon-base>
           <icon-base width="30" height="30" viewBox="0 0 100 100" icon-name="wordpress">
             <icon-wordpress />
           </icon-base>
-          <icon-base width="30" height="30" viewBox="0 0 100 100" icon-name="wordpress">
-            <icon-wordpress />
+          <icon-base width="30" height="30" viewBox="0 0 307 356" icon-name="greensock-gsap">
+            <icon-greensock />
+          </icon-base>
+          <icon-base width="30" height="30" viewBox="0 0 256 256" icon-name="mapbox">
+            <icon-mapbox />
+          </icon-base>
+          <icon-base width="30" height="30" viewBox="0 0 256 256" icon-name="bootstrap">
+            <icon-bootstrap />
           </icon-base>
         </div>
+        <hooper ref="carouselErinnerungsort" :settings="settings" class="three-device-carousel">
+          <slide class="mockup-slide is-flex hcenter">
+            <video-mockup-mobile class="desktop-mockup is-hidden-tablet"
+              videoUrl=""
+              videoType="video/mp4"
+              posterUrl="sp-widescreen-poster"
+              width="300px"
+              height="156.41px"
+            />
+            <video-mockup-mobile class="desktop-mockup is-hidden-mobile is-hidden-widescreen"
+              videoUrl=""
+              videoType="video/mp4"
+              posterUrl="sp-widescreen-poster"
+              width="767.2px"
+              height="400px"
+            />
+            <video-mockup-mobile class="desktop-mockup is-hidden-touch is-hidden-desktop-only"
+              videoUrl=""
+              videoType="video/mp4"
+              posterUrl="sp-widescreen-poster"
+              width="959px"
+              height="500px"
+            />
+          </slide>
+          <slide class="mockup-slide is-flex hcenter">
+            <video-mockup-mobile class="tablet-mockup is-hidden-tablet"
+              videoUrl=""
+              videoType="video/mp4"
+              posterUrl="sp-tablet-poster"
+              width="300px"
+              height="225px"
+            />
+            <video-mockup-mobile class="tablet-mockup is-hidden-mobile is-hidden-widescreen"
+              videoUrl=""
+              videoType="video/mp4"
+              posterUrl="sp-tablet-poster"
+              width="533.33px"
+              height="400px"
+            />
+            <video-mockup-mobile class="tablet-mockup is-hidden-touch is-hidden-desktop-only"
+              videoUrl=""
+              videoType="video/mp4"
+              posterUrl="sp-tablet-poster"
+              width="666.66px"
+              height="500px"
+            />
+          </slide>
+          <slide class="mockup-slide is-flex hcenter">
+            <video-mockup-mobile class="mobile-mockup is-hidden-desktop"
+              videoUrl=""
+              videoType="video/mp4"
+              posterUrl="sp-mobile-poster"
+              width="200px"
+              height="400px"
+            />
+            <video-mockup-mobile class="mobile-mockup is-hidden-touch"
+              videoUrl=""
+              videoType="video/mp4"
+              posterUrl="sp-mobile-poster"
+              width="250px"
+              height="500px"
+            />
+          </slide>
+        </hooper>
+        <div class="is-flex arrow-hoo-nav">
+          <icon-base @click.native="slidePrev" id="erinnerungsort" width="50" height="50" viewBox="0 0 616.49 167.02" icon-name="arrow-back">
+            <icon-arrow-back />
+          </icon-base>
+          <icon-base @click.native="slideNext" id="erinnerungsort" width="50" height="50" viewBox="0 0 616.49 167.02" icon-name="arrow-back">
+            <icon-arrow-back />
+          </icon-base>
+        </div>
+        <h1 class="title title-step is-size-5 is-family-primary">
+            Erinnerungsort 
+        </h1>
+        <p class="is-family-primary has-text-black p-after-h p-anim p-web-desc">
+          Erinnerungsort ist ein Plattform zum Gedenken an Holocaust-Opfer.
+        </p>
+        <div class="tools">
+          <p>Frameworks</p>
+          <icon-base width="30" height="30" viewBox="0 0 256 290" icon-name="vue-logo">
+            <icon-webpack />
+          </icon-base>
+          <icon-base width="30" height="30" viewBox="0 0 256 221" icon-name="vue-logo">
+            <icon-vue />
+          </icon-base>
+          <icon-base width="30" height="30" viewBox="0 0 256 256" icon-name="mapbox">
+            <icon-mapbox />
+          </icon-base>
+        </div>
+        <hooper ref="carouselErinnerungsort" :settings="settings" class="three-device-carousel">
+          <slide class="mockup-slide is-flex hcenter">
+            <video-mockup-mobile class="mobile-mockup is-hidden-desktop"
+              videoUrl=""
+              videoType="video/mp4"
+              posterUrl="sp-mobile-poster"
+              width="200px"
+              height="400px"
+            />
+            <video-mockup-mobile class="mobile-mockup is-hidden-touch"
+              videoUrl=""
+              videoType="video/mp4"
+              posterUrl="sp-mobile-poster"
+              width="250px"
+              height="500px"
+            />
+          </slide>
+        </hooper>
+        <div class="is-flex arrow-hoo-nav">
+          <icon-base @click.native="slidePrev" id="pickme" width="50" height="50" viewBox="0 0 616.49 167.02" icon-name="arrow-back">
+            <icon-arrow-back />
+          </icon-base>
+          <icon-base @click.native="slideNext" id="pickme" width="50" height="50" viewBox="0 0 616.49 167.02" icon-name="arrow-back">
+            <icon-arrow-back />
+          </icon-base>
+        </div>
+        <h1 class="title title-step is-size-5 is-family-primary">
+            Pick Me – Deine Packliste
+        </h1>
+        <p class="is-family-primary has-text-black p-after-h p-anim p-web-desc">
+          Mit der Pick Me App können Gepäckstücke wie Koffer und Rucksäcke angelegt und deren Inhalte verwaltet werden.
+        </p>
+        <div class="tools">
+          <p>Frameworks</p>
+          <icon-base width="30" height="30" viewBox="0 0 256 290" icon-name="vue-logo">
+            <icon-webpack />
+          </icon-base>
+          <icon-base width="30" height="30" viewBox="0 0 256 221" icon-name="vue-logo">
+            <icon-vue />
+          </icon-base>
+        </div>
+
+
+
+
+
       </div>
     </section>
 </template>
@@ -104,6 +249,11 @@
 <script>
 import IconBase from '../components/IconBase.vue'
 import IconWordpress from '../components/icons/IconWordpress.vue'
+import IconGreensock from '../components/icons/IconGreensock.vue'
+import IconMapbox from '../components/icons/IconMapbox.vue'
+import IconVue from '../components/icons/IconVue.vue'
+import IconWebpack from '../components/icons/IconWebpack.vue'
+import IconBootstrap from '../components/icons/IconBootstrap.vue'
 import IconArrowBack from '../components/icons/IconArrowBack.vue'
 import {
   Hooper,
@@ -117,6 +267,11 @@ export default {
   components: {
     IconBase,
     IconWordpress,
+    IconGreensock,
+    IconMapbox,
+    IconVue,
+    IconWebpack,
+    IconBootstrap,
     IconArrowBack,
     Hooper,
     Slide,
@@ -142,11 +297,19 @@ export default {
     }
   },
   methods: {
-    slidePrev() {
-      this.$refs.carousel.slidePrev();
+    slidePrev(event) {
+      if (event.target.id === "sweety-pretty") {
+        this.$refs.carouselSp.slidePrev();  
+      } else if (event.target.id === "erinnerungsort") {
+        this.$refs.carouselErinnerungsort.slidePrev();
+      }
     },
     slideNext() {
-      this.$refs.carousel.slideNext();
+      if (event.target.id === "sweety-pretty") {
+        this.$refs.carouselSp.slideNext();  
+      } else if (event.target.id === "erinnerungsort") {
+        this.$refs.carouselErinnerungsort.slideNext();
+      }
     }
   }
 }
---
+++ src/components/icons/IconVue.vue
@@ -0,0 +1,13 @@
+<template>
+  <g>
+	<path d="M204.8,0H256L128,220.8L0,0h50.6h47.4L128,51.2L157.4,0H204.8z"/>
+	<path class="st1" d="M0,0l128,220.8L256,0h-51.2L128,132.5L50.6,0H0z"/>
+	<path class="st2" d="M50.6,0L128,133.1L204.8,0h-47.4L128,51.2L97.9,0H50.6z"/>
+  </g>
+</template>
+
+<style scoped>
+
+.st1{fill:rgb(167, 167, 167);}
+.st2{fill:#3f3f3f;}
+</style>
