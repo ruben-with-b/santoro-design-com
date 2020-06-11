deac9034973330b1213caf400338ca9eb41ed86b
rub-n <ruben.santoro@hs-augsburg.de>
2020-06-11 18:11:39 +0200

    finilize pre-rendering setup

--- vue.config.js
+++ vue.config.js
@@ -6,5 +6,19 @@ module.exports = {
             .use("vue-svg-inline-loader")
                 .loader("vue-svg-inline-loader")
                 .options({ /* ... */ });
+    },
+
+    pluginOptions: {
+      prerenderSpa: {
+        registry: undefined,
+        renderRoutes: [
+          '/',
+          '/services',
+          '/services/websites-and-apps'
+        ],
+        useRenderEvent: false,
+        headless: true,
+        onlyProduction: true
+      }
     }
 };
