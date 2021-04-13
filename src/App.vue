d70024a2a796e2394e5ba3ef442f281826e17dfd
rub-n <ruben.santoro@hs-augsburg.de>
2021-04-13 16:24:48 +0200

    add cookie banner

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,30 @@
 <template>
   <div id="app">
     <router-view></router-view>
+      <cookie-law
+        message="Ich liebe Cookies 🍪 Sie sorgen dafür, dass diese Website sauber
+          läuft und ich über deinen Besuch informiert werde."
+        buttonText="Ok, passt!"
+        buttonLink="/privacy"
+        buttonLinkText="Datenschutzerklärung"
+        buttonClass="santoro-cookie-button"
+      >
+      </cookie-law>
   </div>
 </template>
 
 <script>
+import CookieLaw from 'vue-cookie-law'
 
 export default {
-  name: 'app'
+  name: 'app',
+    components: { CookieLaw }
 }
 </script>
 
 <style lang="scss">
-
+.Cookie{
+  background: #f69f19;
+}
 
 </style>
