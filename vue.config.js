be8c944ca75c759573c44af84d8671cd905a1f58
rub-n <ruben.santoro@hs-augsburg.de>
2021-04-13 20:24:50 +0200

    minor fix

--- vue.config.js
+++ vue.config.js
@@ -1,32 +1,26 @@
-var PrerenderSpaPlugin = require('prerender-spa-plugin')
-var path = require('path')
-
 module.exports = {
     chainWebpack: config => {
-      config.module
-          .rule("vue")
-          .use("vue-svg-inline-loader")
-              .loader("vue-svg-inline-loader")
-              .options({ /* ... */ });
-    
-      
-      if (process.env.NODE_ENV !== 'production') return
-      
-      return {
-        plugins: [
-          new PrerenderSpaPlugin(
-            // Absolute path to compiled SPA
-            path.resolve(__dirname, 'dist'),
-            // List of routes to prerender
-            [ '/',
-              '/about',
-              '/services',
-              '/services/websites-and-apps',
-              '/services/print',
-              '/services/corporate-design',
-            ],
-          ),
-        ]
+       
+        config.module
+            .rule("vue")
+            .use("vue-svg-inline-loader")
+                .loader("vue-svg-inline-loader")
+                .options({ /* ... */ });
+    },
+
+    pluginOptions: {
+      prerenderSpa: {
+        registry: undefined,
+        renderRoutes: [
+          '/',
+          '/services',
+          '/services/websites-and-apps',
+          '/services/print',
+          '/services/corporate-design'
+        ],
+        useRenderEvent: false,
+        headless: true,
+        onlyProduction: true
       }
     }
 };
